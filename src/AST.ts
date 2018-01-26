d576c26d3fcd8db8f20ce3c2e9ab69ec5d8a99fc
rozzzly <pclienau@gmail.com>
2018-01-25 18:33:09 -0600

    designed AST

--- src/AST.ts
+++ src/AST.ts
@@ -9,16 +9,17 @@ export interface Range {
     stop: Location;
 }
 
-export interface Node<T in NodeTypes = NodeTypes> {
+export interface Node<T extends string = NodeTypes> {
     type: T;
     loc: Range;
     parent: Node;
     raw: string;
 }
 
+
+
 export type Nodes = (
     | RootNode
-    | TextNode
     | PlainTextNode
     | AnsiTextNode
     | CharacterNode
@@ -28,7 +29,7 @@ export type Nodes = (
 
 export type NodeTypes = (
     | 'RootNode'
-    | 'TextNode'
+    // | 'TextNode'
     | 'PlainTextNode'
     | 'AnsiTextNode'
     | 'CharacterNode'
@@ -36,7 +37,7 @@ export type NodeTypes = (
     | 'AnsiEscapeNode'
 );
 
-export type NodeLookup<T in NodeTypes> = {
+export type NodeTypeLookup = {
     
     TextNode: (
         | 'PlainTextNode'
@@ -47,14 +48,112 @@ export type NodeLookup<T in NodeTypes> = {
         | 'NewLineNode'
         | 'AnsiEscapeNode'
     );
-    ''
+    // NewLineNode: NodeLineNode;
 };
 
 
-export class BaseNode<T in NodeTypes = NodeTypes> implements Node<T> {
+export class BaseNode<T extends NodeTypes> implements Node<T> {
     public type: T;
     public loc: Range;
-    public parent: Node
+    public relativeLoc: Range;
+    public parent: Node;
+    public raw: string;
+    public constructor(parent: Node) {
+        this.parent = parent;
+    }
 }
 
-export class 
+export type TextNode = (
+    | PlainTextNode
+    | AnsiTextNode
+);
+
+export type TextNodeTypes = (
+    | 'PlainTextNode'
+    | 'AnsiTextNode'
+);
+
+export class RootNode extends BaseNode<'RootNode'> {
+    public children: TextNode[];
+    public derivedFrom?: RootNode;
+    public constructor() {
+        super(undefined);
+    }
+    public splitMultiLine(): this {
+        if (this.derivedFrom) {
+            throw new ReferenceError();
+        } else {
+            return undefined;
+        }
+    }
+}
+
+export abstract class BaseTextNode<T extends TextNodeTypes> extends BaseNode<T> {
+    public parent: RootNode;
+    public children: VisibleCharacterNode[];
+    public derivedFrom?: this;
+    public constructor(parent: RootNode, children: VisibleCharacterNode[]) {
+        super(parent);
+        this.children = children;
+    }
+
+    public get lines(): CharacterNode[][] {
+        const result: CharacterNode[][] = [];
+        for(let i = 0, l = 0; i < this.children.length; i++) {
+            const current = this.children[i];
+            if (current.type === 'NewLineNode') {
+                l++;
+            } else {
+                result[l].push(current);
+            }
+        }
+        return result;
+    }
+    public abstract splitMultiLine(): this[];
+}
+
+export class PlainTextNode extends BaseTextNode<'PlainTextNode'> { 
+
+    public splitMultiLine(): this[] {
+        throw new Error("Method not implemented.");
+    }
+}
+
+export class AnsiTextNode extends BaseTextNode<'AnsiTextNode'> {
+    public splitMultiLine(): this[] {
+        throw new Error("Method not implemented.");
+    }
+    public open: AnsiEscapeNode;
+    public close: AnsiEscapeNode;
+}
+
+export type CharacterNodeTypes = (
+    | 'CharacterNode'
+    | 'NewLineNode'
+    | 'AnsiEscapeNode'
+);
+
+export type VisibleCharacterNode = (
+    | CharacterNode
+    | NewLineNode
+);
+
+export type VisibleCharacterNodeTypes = (
+    | 'CharacterNode'
+    | 'NewLineNode'
+);
+export class CharacterNode<T extends CharacterNodeTypes = 'CharacterNode'> extends BaseNode<T> {
+    public value: string;
+
+    public constructor(parent: TextNode, value: string) {
+        super(parent);
+        this.value = value;
+    }
+}
+
+export class NewLineNode extends CharacterNode<'NewLineNode'> {
+
+}
+export class AnsiEscapeNode extends CharacterNode<'AnsiEscapeNode'> {
+
+}
