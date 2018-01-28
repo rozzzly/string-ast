942d46d6c1861a6a4f082d4312a319af9a7d2280
rozzzly <pclienau@gmail.com>
2018-01-28 17:55:45 -0600

    restructure `AnsiTextNode` children to include `AnsiEscapeNode`

--- src/AST.ts
+++ src/AST.ts
@@ -2,6 +2,7 @@ import { AnsiStyle } from "./AnsiStyle";
 
 export interface Location {
     offset: number;
+    textOffset: number;
     line: number;
     column: number;
 }
@@ -10,7 +11,7 @@ export interface CompoundLocation extends Location {
     offset: number; // global
     line: number; // global
     column: number; // global
-    relative: Location // relative to parent
+    relative: Location; // relative to parent
 }
 
 export interface Range {
@@ -31,8 +32,6 @@ export type Nodes = (
     | NewLineNode
     | AnsiEscapeNode
 );
-
-
 
 export type NodeTypes = (
     | 'RootNode'
@@ -71,6 +70,7 @@ export type TextNodeTypes = (
 );
 
 export class RootNode extends Node<'RootNode'> implements HasRaw {
+    public type: 'RootNode' = 'RootNode';
     public raw: string;
     public children: TextNode[];
     public range: Range;
@@ -88,43 +88,45 @@ export class RootNode extends Node<'RootNode'> implements HasRaw {
         let line: number = 0;
         let column: number = 0;
         let offset: number = 0;
+        let textOffset: number = 0;
         this.range = {
-            start: { line, column, offset },
+            start: { line, column, offset, textOffset },
             stop: undefined
         };
         this.children.forEach(child => {
-            child.calculateRange({ line, column, offset });
+            child.calculateRange({ line, column, offset, textOffset });
             line = child.range.stop.line;
             column = child.range.stop.column;
             offset = child.range.stop.offset;
+            textOffset = child.range.stop.textOffset;
         });
-        this.range.stop = { line, column, offset };
+        this.range.stop = { line, column, offset, textOffset };
     }
 }
 
 export abstract class BaseTextNode<T extends TextNodeTypes> extends Node<T> implements HasRaw {
+    public abstract type: T;
     public raw: string;
     public parent: RootNode;
-    public children: VisibleCharacterNode[];
+    public children: TextUnitNode[];
     public range: Range;
-    public constructor(parent: RootNode, children: VisibleCharacterNode[]) {
+    public constructor(parent: RootNode, children: TextUnitNode[]) {
         super(parent);
         this.children = children;
     }
 
-    public get lines(): CharacterNode[][] {
-        const result: CharacterNode[][] = [];
-        for(let i = 0, l = 0; i < this.children.length; i++) {
-            const current = this.children[i];
-            if (current.type === 'NewLineNode') {
-                l++;
-            } else {
-                result[l].push(current);
-            }
-        }
-        return result;
-    }
-    public abstract splitMultiLine(): this[];
+    // public get lines(): CharacterNode[][] {
+    //     const result: CharacterNode[][] = [];
+    //     for(let i = 0, l = 0; i < this.children.length; i++) {
+    //         const current = this.children[i];
+    //         if (current.type === 'NewLineNode') {
+    //             l++;
+    //         } else {
+    //             result[l].push(current);
+    //         }
+    //     }
+    //     return result;
+    // }
 
     public calculateRange(parentOffset: Location) {
         this.range = {
@@ -136,20 +138,25 @@ export abstract class BaseTextNode<T extends TextNodeTypes> extends Node<T> impl
         let line: number = 0;
         let column: number = 0;
         let offset: number = 0;
+        let textOffset: number = 0;
         this.children.forEach(child => {
             child.range = {
                 start: {
                     line: parentOffset.line + line,
                     column: parentOffset.column + column,
                     offset: parentOffset.offset + offset,
+                    textOffset: parentOffset.textOffset + textOffset,
                     relative: {
-                        line, column, offset
+                        line, column, offset, textOffset
                     }
                 },
                 stop: undefined
             };
             column += child.width;
             offset += child.bytes;
+            if (child.type !== 'AnsiEscapeNode') {
+               textOffset += child.bytes; 
+            }
             if (child.type === 'NewLineNode') {
                 line++;
                 column = 0;
@@ -158,8 +165,9 @@ export abstract class BaseTextNode<T extends TextNodeTypes> extends Node<T> impl
                 line: parentOffset.line + line,
                 column: parentOffset.column + column,
                 offset: parentOffset.offset + offset,
+                textOffset: parentOffset.textOffset + offset,
                 relative: {
-                    line, column, offset
+                    line, column, offset, textOffset
                 }
             };
         });
@@ -171,70 +179,74 @@ export abstract class BaseTextNode<T extends TextNodeTypes> extends Node<T> impl
             this.range.stop = {
                 offset: this.range.start.offset + offset,
                 column: this.range.start.column + column,
-                line: this.range.start.line + line
+                line: this.range.start.line + line,
+                textOffset: this.range.start.textOffset + textOffset
             };
         }
     }
 }
 
 export class PlainTextNode extends BaseTextNode<'PlainTextNode'> { 
-
-    public splitMultiLine(): this[] {
-        throw new Error("Method not implemented.");
-    }
+    public type: 'PlainTextNode' = 'PlainTextNode';
 }
 
 export class AnsiTextNode extends BaseTextNode<'AnsiTextNode'> {
+    public type: 'AnsiTextNode' = 'AnsiTextNode';
     public style: AnsiStyle;
 
-    public constructor(parent: RootNode, children: VisibleCharacterNode[], style: AnsiStyle) {
+    public constructor(parent: RootNode, children: TextUnitNode[], style: AnsiStyle) {
         super(parent, children);
         this.style = style;
     }
 
-    public splitMultiLine(): this[] {
-        throw new Error("Method not implemented.");
+    public get relatedEscapes(): { before: AnsiEscapeNode[], after: AnsiEscapeNode[] }  {
+        const before: AnsiEscapeNode[] = [];
+        const after: AnsiEscapeNode[] = [];
+        return { before, after }; 
     }
 }
 
-export type CharacterNodeTypes = (
-    | 'CharacterNode'
-    | 'NewLineNode'
-    | 'AnsiEscapeNode'
-);
 
-export type VisibleCharacterNode = (
+export type TextUnitNode = (
+    | AnsiEscapeNode
     | CharacterNode
     | NewLineNode
 );
 
-export type VisibleCharacterNodeTypes = (
+export type TextUnitNodeTypes = (
+    | 'AnsiEscapeNode'
     | 'CharacterNode'
     | 'NewLineNode'
 );
-export class CharacterNode<T extends CharacterNodeTypes = 'CharacterNode'> extends Node<T> {
+
+export abstract class BaseTextUnitNode<T extends TextUnitNodeTypes> extends Node<T> {
+    public abstract type: T;
     public value: string;
     public width: number;
     public bytes: number;
     public range: CompoundRange;
+}
+
+export class CharacterNode extends BaseTextUnitNode<'CharacterNode'> {
+    public type: 'CharacterNode' = 'CharacterNode';
     public constructor(parent: TextNode, value: string) {
         super(parent);
         this.value = value;
     }
 }
 
-export class NewLineNode extends CharacterNode<'NewLineNode'> {
-    public width: 0;
-    public constructor(parent: TextNode, value: string) {
-        super(parent, value);
-        this.width = 0;
+export class AnsiEscapeNode extends BaseTextUnitNode<'AnsiEscapeNode'> {
+    public type: 'AnsiEscapeNode' = 'AnsiEscapeNode';
+    public width: 0 = 0;
+    public constructor(parent: TextNode, params: number[]) {
+        super(parent);
     }
 }
-export class AnsiEscapeNode extends CharacterNode<'AnsiEscapeNode'> {
-    public params: number[];
-    public width: 0;
+
+export class NewLineNode extends BaseTextUnitNode<'NewLineNode'> {
+    public type: 'NewLineNode' = 'NewLineNode';
+    public width: 0 = 0;
     public constructor(parent: TextNode, value: string) {
-        super(parent, value);
-        this.width = 0;
+        super(parent);
     }
 }
