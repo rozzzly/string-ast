6d7ed746347ba94cdfb8c0ef537f0526b13b5d82
rozzzly <pclienau@gmail.com>
2018-02-27 17:17:08 -0600

    prevent double wrapping of children

--- src/AST/navigation.ts
+++ src/AST/navigation.ts
@@ -1,15 +1,28 @@
 import { NodeKind, Node, NodeLookup } from '../AST';
 import { inRange } from '../misc';
 
+export const IsChildren: unique symbol = Symbol('[string-ast]::AST/navigation.Children');
+export type IsInvalidated = typeof IsChildren;
+
+
 export interface Children<U> extends Array<U> {
     get<T extends U = U>(index: number): T;
+    [IsChildren]: true;
 }
 
 export function wrapChildren<U>(): Children<U>;
 export function wrapChildren<U>(children: U[]): Children<U>;
 export function wrapChildren<U>(children: U[] = []): Children<U> {
-    const result: Children<U> = children as any;
+    // prevent double wrapping
+    if ((children as any)[IsChildren]) return children as any;
 
+    // prevent mutations
+    const result: Children<U> = [...children] as any;
+
+    Object.defineProperty(result, IsChildren, {
+        enumerable: false,
+        value: true
+    });
     Object.defineProperty(result, 'get', {
         enumerable: false,
         value: <T extends U>(index: number): T => {
