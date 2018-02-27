e6e55f221c823f86d2b9925bf379b820dab70baa
rozzzly <pclienau@gmail.com>
2018-02-27 15:26:34 -0600

    start working on loop body for `sliceByPlainTextBytes`

--- src/tools/slice.ts
+++ src/tools/slice.ts
@@ -3,6 +3,9 @@ import { previousNodeOfKind } from '../AST/navigation';
 import { NewLineEscapeNode } from '../AST/TextChunkNode/NewLineEscapeNode';
 import { CharacterNode } from '../AST/TextChunkNode/CharacterNode';
 import { Location } from '../AST/Location';
+import { Range } from '../AST/Range';
+import { inRange } from '../misc';
+import { TextChunkNode } from '../AST/TextChunkNode';
 
 export type BadSliceStrategy = (
     | 'throw'
@@ -27,10 +30,37 @@ export function sliceByPlainTextBytes(root: RootNode, start: number, end?: numbe
             end_safe = end;
         }
     }
+    // ensure stop/start is valid
+    if (!inRange(0, rightBound, start_safe)) throw new RangeError();
+    if (!inRange(0, rightBound, end_safe)) throw new RangeError();
 
-    let startSpan;
+    const nRoot = root.clone();
+    const cursor: { span: number, chunk: number, offset: number } = { span: 0, chunk: 0, offset: 0 };
+
+    while (cursor.offset < end_safe) {
+        if (cursor.span >= root.children.length) throw new RangeError();
+        else {
+            const currentSpan = root.children.get(cursor.span);
+            if (cursor.offset >= currentSpan.range.start.plainTextOffset) {
+                // entire span is inside desired range, clone and save it.
+                if (cursor.offset <= currentSpan.range.stop.plainTextOffset) {
+                    nRoot.children.push(currentSpan.clone(nRoot));
+                    cursor.offset = currentSpan.range.stop.plainTextOffset;
+                    cursor.span++;
+                } else {
+                    const included: TextChunkNode[] = [];
+                    const chunks = currentSpan.children;
+                    let currentChunk = chunks.get(0);
+                    while (cursor.offset < currentChunk.range.stop.plainTextOffset) {
+                        included.push(currentChunk);
+                    }
+                }
+            }
+        }
 
 
+
+    }
 
     return root;
 }
