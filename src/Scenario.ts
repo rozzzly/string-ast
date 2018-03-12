a7761909c14ac53a10dd7cbe653feede633b4d0d
rozzzly <pclienau@gmail.com>
2018-03-12 05:35:52 -0500

    `Scenario` finally working how I like it!!

--- src/Scenario.ts
+++ src/Scenario.ts
@@ -14,106 +14,79 @@ export type DiscriminateUnion<
     )
 );
 
-export type lookup<P extends Constructor<{ name: string }>[], N extends Instance<P[number]>['name']> = DiscriminateUnion<Instance<P[number]>, 'name', N>;
-
-export type Proposals2<C extends  Constructor<{ name: string }>> = {
+export type Proposals<C extends Constructor<{ name: string }>> = {
     [N in Instance<C>['name']]: Deconstruct<C, DiscriminateUnion<Instance<C>, 'name', N>>;
 };
 
-export interface Proposals {
-    [name: string]: Constructor<{ name: string }>;
-}
+export type Scenario<C extends Constructor<{ name: string }>> = (
+    (
+        Proposals<C>
+    ) & {
+        enact<K extends keyof Proposals<C>, I extends Instance<Proposals<C>[K]>>(value: I): I;
+        enact<K extends keyof Proposals<C>>(value: K): Instance<Proposals<C>[K]>;
+    }
+);
 
-type test = Proposals2<[Foo, Bar]>;
-
-export interface Scenario2<C extends Constructor<{ name: string }>> {
-    name: string;
-    plans:  Proposals2<C>;
-    enact<K extends keyof Proposals2<C>, I extends Instance<Proposals2<C>[K]>>(value: I): I;
-    enact<K extends keyof Proposals2<C>>(value: K): Instance<Proposals2<C>[K]>;
-}
-export interface Scenario<P extends Proposals> {
-    name: string;
-    plans: P;
-    enact<K extends keyof P, I extends Instance<P[K]>>(value: I): I;
-    enact<K extends keyof P>(value: K): Instance<P[K]>;
-}
 
 export type Implementation<S> = (
     (S extends Scenario<infer P> // extract Proposals
         ? (
-            | keyof P
-            | Instance<P[keyof P]>
+            | Instance<P>['name']
+            | Instance<P>
         )
         : never
     )
 );
 
 class Foo {
+    public static something: boolean = false;
     public name: 'Foo' = 'Foo';
+    constructor(v: number) {
+        console.log(v);
+    }
 }
 class Bar {
     public name: 'Bar' = 'Bar';
 }
-const p = { Foo, Bar };
-const s = scenario('', p);
-s.enact(new Foo());
 
+export function scenario<C extends Constructor<{ name: string }>>(plans: C[]): Scenario<C> {
+    const aliases: (keyof Proposals<C>)[] = [];
+    const planMap: any = {};
 
-
-type die = lookup<[typeof Foo, typeof Bar], 'Foo'>;
-
-function hello<P extends Constructor<{ name: string }>[], N extends Instance<P[number]>['name']>(z: P, i: N):   {
-    return undefined;
-}
-
-
-const loo = hello([Foo, Bar]);
-
-
-
-export function scenario<C extends Constructor<{ name: string }>>(name: string, plans: C[]): Scenario2<C> {
-    const aliases: (keyof Proposals2<C>)[] = [];
-    const planMap = Proposals.
-    
-    plans.reduce((reduction, plan) => ({
-        ...reduction,
-        [new plan().name]: plan
-    }), [{}) as (Proposals2<C>);
+    plans.forEach(PlanConstructor => {
+        const planName = (new PlanConstructor()).name;
+        if (typeof planName !== 'string') {
+            throw TypeError('A plan must have a (string) name!!');
+        } else if (planName === '') {
+            throw TypeError('A Plan must have a name!!');
+        } else if (planName === 'enact') {
+            throw TypeError('A Plan cannot be named `enact`!');
+        } else if (aliases.includes(planName)) {
+            throw TypeError('A Plan with this name already exists!');
+        } else {
+            planMap[planName] = PlanConstructor;
+            aliases.push(planName);
+        }
+    });
     return {
-        plans: planMap,
-        name: name,
+        ...planMap,
         enact(value: any) { // type signature defined in `Scenario` interface
-            if (typeof value === 'string') {
-                if (aliases.includes(value)) {
-                    return new planMap[value as keyof Proposals2<C>]();
+            if (typeof value === 'string' && value !== 'enact') {
+                const ClassConstructor = planMap[value];
+                if (ClassConstructor) {
+                    return new ClassConstructor();
                 } else {
-                    throw new TypeError();
+                    throw new TypeError('Unregistered Plan name');
                 }
             } else {
-                // we can probably save some time by checking things with matching names
-                const className = value.constructor.name;
-                if (aliases.includes(className)) {
-                    const ClassConstructor = plans[className as keyof P];
-                    if (value instanceof ClassConstructor) {
-                        return value;
-                    }
-                }
-                // check to see if its an instance of _any_ of the predefined constructors
-                for (let alias of aliases) {
-                    const ClassConstructor = plans[alias];
-                    if (value instanceof ClassConstructor) {
-                        return value;
-                    }
+                if (aliases.includes(value.name) && value instanceof planMap[value.name]) {
+                    return value;
+                } else {
+                    // we were not given an instance of one of the predefined constructors
+                    throw new TypeError('Supplied neither name of nor instance of a registered Plan');
                 }
 
-                // we were not given an instance of one of the predefined constructors
-                throw new TypeError();
             }
         }
     };
 }
-
-const derp = scenario('hi', [Foo, Bar]);
-
-const nDerp = new derp.plans.Bar();
